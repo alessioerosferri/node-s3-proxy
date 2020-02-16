02a9ef9dfcc8865447915b46f9219eda59fc7789
Alessio <alessioferriweb@gmail.com>
2020-02-16 16:36:07 +0000

    handling errors whilst fetching assets

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ const app = express();
 const PORT = 3000;
 
 app.get("/:bucket/(*/:key|:key)", (req, res) => {
-    let {bucket, key} = req.params;
+    try{
+        let {bucket, key} = req.params;
 
-    let s3 = new AWS.S3({
-        accessKeyId: process.env.S3_KEY,
-        secretAccessKey: process.env.S3_SECRET,
-        // endpoint: process.env.HOST ,
-        // s3ForcePathStyle: true, // needed with minio?
-        signatureVersion: 'v4'
-    });
-    s3.getObject({Bucket: bucket, Key: key, Range: "bytes=0-"}, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else{
-            if (data.ContentType.includes("gzip")){
-                res.set('Content-Encoding', 'gzip');
+        let s3 = new AWS.S3({
+            accessKeyId: process.env.S3_KEY,
+            secretAccessKey: process.env.S3_SECRET,
+            // endpoint: process.env.HOST ,
+            // s3ForcePathStyle: true, // needed with minio?
+            signatureVersion: 'v4'
+        });
+        s3.getObject({Bucket: bucket, Key: key, Range: "bytes=0-"}, function(err, data) {
+            if (err) res.status(404).send(); // an error occurred
+            else{
+                if (data.ContentType.includes("gzip")){
+                    res.set('Content-Encoding', 'gzip');
+                }
+                res.send(data.Body);
             }
-            res.send(data.Body);
-        }
-    });
+        });
+    }
+    catch (e) {
+        console.error(e);
+        res.status(500).send();
+    }
+
 });
 
 app.listen(PORT, () => {
